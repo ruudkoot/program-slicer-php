8fac97108901e3328979ce4704fb9616ac99410b
Ruud Koot <inbox@ruudkoot.nl>
2010-06-04 12:44:16 +0000

    type error freeness\!

--- src/MF/DirectlyRelevantVariables.hs
+++ src/MF/DirectlyRelevantVariables.hs
@@ -1,5 +1,4 @@
 {-# LANGUAGE MultiParamTypeClasses  #-}
-{-# LANGUAGE FunctionalDependencies #-}
 {-# LANGUAGE TypeSynonymInstances   #-}
 {-# LANGUAGE ScopedTypeVariables    #-}
 
--- src/MF/ProgramSlicing.hs
+++ src/MF/ProgramSlicing.hs
@@ -1,13 +1,16 @@
 module MF.ProgramSlicing where
 
-import qualified Data.Set as Set
 import MF.DirectlyRelevantVariables
 import MF.Program
 import MF.Analysis
 
 import Data.Maybe
-import qualified Data.IntMap as IM
+
+import qualified Data.Set as Set
 import qualified Data.Map as    Map
+import qualified Data.IntSet as IntSet
+import qualified Data.IntMap as IM
+
 
 import qualified Data.Graph.Inductive as G
 import qualified Data.GraphViz as GV
@@ -22,6 +25,7 @@ type SlicingContext = (Map.Map CallContext (Set.Set Label) --Set of relevant sta
 
 
 -- | Calculate control statements that are directly relevant to their respective variables
+{-
 directlyRelevantStatements :: Program -> Values SymbolType -> Set.Set Label
 directlyRelevantStatements program relevantVariables = Set.fromList $ map fst $ filter isRelevant (flow program)
         where
@@ -33,12 +37,13 @@ directlyRelevantStatements program relevantVariables = Set.fromList $ map fst $ 
                                 
                                 b :: Set.Set SymbolType 
                                 b = modified (statementAt program i)
+-}
 
 
-directlyRelevantStatements :: Program -> Values SymbolType -> M.Map CallContext (Set.Set Label)
-directlyRelevantStatements program relevantVariables = Map.fromList (map (\context -> (context, forEachContext context)) callContexts
+directlyRelevantStatements :: Program -> Values SymbolType -> Map.Map CallContext (Set.Set Label)
+directlyRelevantStatements program relevantVariables = Map.fromList (map (\context -> (context, forEachContext context)) callContexts)
         where
-                callContexts = Set.fromList $ contexts relevantVariables
+                callContexts = Set.toList $ contexts relevantVariables
                 
                 forEachContext :: CallContext ->  Set.Set Label
                 forEachContext context = Set.fromList $ map fst $ filter isRelevant (flow program)
@@ -47,27 +52,27 @@ directlyRelevantStatements program relevantVariables = Map.fromList (map (\conte
                         isRelevant (i,j) = (not . Set.null) $ Set.intersection a b
                             where 
                                 a :: Set.Set SymbolType 
-                                a = fromJust (M.lookup context (fromJust (IM.lookup j relevantVariables)))
+                                a = fromJust (Map.lookup context (fromJust (IM.lookup j relevantVariables)))
                                 
                                 b :: Set.Set SymbolType 
                                 b = modified (statementAt program i)
                                 
 
 -- | Determine which control statements are relevant to the existing relevant statements
-controlDependentBranchStatements :: Program -> M.Map CallContext (Set.Set Label) -> M.Map CallContext (Set.Set Label)
-controlDependentBranchStatements program = M.map forEachContext
-        where
-                forEachContext releventStatements = Map.keysSet $ Map.filter inRange (rangeOfInfluence program)
+controlDependentBranchStatements :: Program -> Map.Map CallContext (Set.Set Label) -> Map.Map CallContext (Set.Set Label)
+controlDependentBranchStatements program = Map.map forEachContext
+        where   forEachContext :: Set.Set Label -> Set.Set Label
+                forEachContext relevantStatements = Set.fromList $ IntSet.toList $ IM.keysSet $ IM.filter inRange (rangeOfInfluence program)
                     where  inRange :: Set.Set Label -> Bool
-                                inRange relevant range = (not . Set.null) $ Set.intersection range relevantStatements
+                           inRange range = (not . Set.null) $ Set.intersection range relevantStatements
 
 --The actual slicing
 backwardsProgramSlicing :: Program -> SlicingContext
 backwardsProgramSlicing program = fixPoint (internalBackwardsProgramSlicing program) (relevantStatements,relevantVariables)
         where   --TODO: Extend to multiple trace statements
-                (startLabel, startValue) = head.Map.toList $ traceStatements program
+                (startLabel, startValue) = head.IM.toList $ traceStatements program
 
-                 analysis = DirectlyRelevantVariables startLabel startValue
+                analysis = DirectlyRelevantVariables startLabel startValue
                 
                 --Start with the directly relevant variables and their statements
                 values = startValues analysis program
@@ -77,6 +82,8 @@ backwardsProgramSlicing program = fixPoint (internalBackwardsProgramSlicing prog
 
 -- Fixpoint function to progress looking for a fixpoint which contians all the control statements.
 internalBackwardsProgramSlicing :: Program -> SlicingContext -> SlicingContext
+
+{-
 internalBackwardsProgramSlicing program (relevantStatements, relevantVariables) = (newRelevantStatements, newRelevantVariables)
          where  --Calculate the new control-dependant statements
                 branchStatements = controlDependentBranchStatements program relevantStatements        
@@ -89,23 +96,27 @@ internalBackwardsProgramSlicing program (relevantStatements, relevantVariables) 
                                                 analysis = DirectlyRelevantVariables right $ referenced $ statementAt program right               
                 
                 newRelevantStatements = Set.union branchStatements $ directlyRelevantStatements program newRelevantVariables          
+-}
+
+internalBackwardsProgramSlicing program (relevantStatements, relevantVariables) = undefined
 
 -- |Produces a list of calls to the trace function and the associated variables.
-traceStatements :: Program -> Map.IntMap (Set.Set SymbolType)
-traceStatements = Map.foldWithKey f (Map.empty) . blocks
+traceStatements :: Program -> IM.IntMap (Set.Set SymbolType)
+traceStatements = IM.foldWithKey f (IM.empty) . blocks
     where
-        f l (FuncCall "trace" vars) r = Map.insert l (Set.fromList vars) r
+        f l (FuncCall "trace" vars) r = IM.insert l (Set.fromList vars) r
         f _ _                       r = r
 
 
 visualizeSlice::Program -> String -> IO ()
+{-
 visualizeSlice program file = 
     let traces = traceStatements program
         (statements,contexts) = backwardsProgramSlicing program
         traceColor = GV.FillColor (GV.RGB 255 0 0)
         useColor   = GV.FillColor (GV.RGB 0   255 0)
         noneColor  = GV.FillColor (GV.RGB 128 128 128)
-        decorateNode (l, n) = [GV.Label (GV.StrLabel (show l ++ ":" ++ show n++" -- "++(show . Set.toList . fromJust .Map.lookup l $ contexts)))
+        decorateNode (l, n) = [GV.Label (GV.StrLabel (show l ++ ":" ++ show n++" -- "++(show . Set.toList . fromJust .IM.lookup l $ contexts)))
                               ,GV.Style [GV.SItem GV.Filled []]
                               ,if Map.member l traces
                                then traceColor
@@ -113,5 +124,6 @@ visualizeSlice program file =
                                     then useColor
                                     else noneColor]
     in visualizeProgramWInfo decorateNode file program
-
+-}
+visualizeSlice program file = undefined
 
