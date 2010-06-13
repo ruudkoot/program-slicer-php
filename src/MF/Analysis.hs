24b9b4af042523eded0fe4bf1e5bd60bb95190b9
Ruud Koot <inbox@ruudkoot.nl>
2010-06-13 19:51:08 +0000

    cleanup

--- src/MF/Analysis.hs
+++ src/MF/Analysis.hs
@@ -6,70 +6,67 @@
 module MF.Analysis where
 
 import           Data.Maybe
-import qualified Data.IntMap   as IM
-import qualified Data.Map      as Map
-import qualified Data.Set   as Set
+import qualified Data.IntMap as IntMap
+import qualified Data.Map    as Map
+import qualified Data.Set    as Set
 
 import MF.Program
 import Debug.Trace
 
 import qualified Data.Graph.Inductive as G
-import qualified Data.GraphViz as GV
+import qualified Data.GraphViz        as GV
 
-type Worklist               = [(Label, Label)]
-type CallContext            = [Label]
-type LabelValues property   = Map.Map CallContext (Set.Set property)       
-type Values property        = IM.IntMap (LabelValues property)
+type Worklist             = [(Label, Label)]
+type CallContext          = [Label]
+type LabelValues property = Map.Map CallContext (Set.Set property)       
+type Values      property = IntMap.IntMap (LabelValues property)
 
 
 contexts :: Values property -> Set.Set CallContext
-contexts = Set.unions . IM.elems . IM.map Map.keysSet
+contexts = Set.unions . IntMap.elems . IntMap.map Map.keysSet
 
-class (Ord property, Show property, Eq property) => Analysis analysis property | analysis -> property where
+class (Ord property, Show property) => Analysis analysis property | analysis -> property where
     flowSelection          :: analysis -> Program -> Flow     
-
     join                   :: analysis -> Set.Set property -> Set.Set property -> Set.Set property
     isMoreInformative      :: analysis -> Set.Set property -> Set.Set property -> Bool
-
     kill                   :: analysis -> Statement -> Set.Set property -> Set.Set property
     generate               :: analysis -> Statement -> Set.Set property -> Set.Set property
-    
+
     transfer               :: analysis -> Statement -> Set.Set property -> Set.Set property
-    transfer          analysis statement input = (input `Set.difference` (kill analysis statement input)) `Set.union` (generate analysis statement input)
+    transfer    analysis statement input  =
+        (input `Set.difference` (kill analysis statement input))
+            `Set.union`
+        (generate analysis statement input)
 
     transferAll            :: analysis -> Program -> Values property -> Values property
-    transferAll       analysis program values = IM.mapWithKey toEffect values
-            where
-                    --toEffect :: Label -> Set.Set property -> Set.Set property
-                    toEffect label = Map.map (transfer analysis (statementAt program label))
+    transferAll analysis program   values = IntMap.mapWithKey toEffect values
+        where toEffect label =
+                Map.map (transfer analysis (statementAt program label))
 
     mergeValues             :: analysis -> Values property -> Values property -> Values property
-    mergeValues analysis = IM.unionWith mergeLabelValues
-        where 
-                mergeLabelValues = Map.unionWith (join analysis)
-
+    mergeValues analysis = IntMap.unionWith mergeLabelValues
+        where mergeLabelValues = Map.unionWith (join analysis)
 
     solve :: analysis -> Program -> Values property -> Values property
     solve analysis program values = 
-                 let flow     = flowSelection analysis program
-                     worklist = flow                     
+                 let worklist = flowSelection analysis program
                      
                      solve' :: analysis -> Program -> Worklist -> Values property -> Values property  
                      solve' analysis program [] values = values
                      solve' analysis program ((start, end):worklistTail) values = 
                          let    --Calculate new values for effect of start 
-                                contextStart    = maybe (error "contextStart") id (IM.lookup start values)
+                                contextStart    = fromMaybe (error "contextStart") (IntMap.lookup start values)
                                 statementStart  = statementAt program start
                                 newEffectStart  = Map.map (transfer analysis statementStart) contextStart
                                 
                                 --Calculate new context values for the end block
-                                oldContextEnd   = maybe (error $ "contexEnd"++show end++show values) id (IM.lookup end values)        
+                                oldContextEnd   = fromMaybe (error $ "contexEnd"++show end++show values) (IntMap.lookup end values)        
                                 ajoin           = Map.unionWith (join analysis)
                                 newContextEnd   = oldContextEnd `ajoin` newEffectStart
                              
                                 --Updated worklist/labelProperties
-                                newWorklist     = worklistTail ++ [(l', l'') | (l', l'') <- flow, l' == end] 
-                                newContexts     = IM.insert end newContextEnd values
+                                newWorklist     = worklistTail ++ [(l', l'') | (l', l'') <- worklist, l' == end] 
+                                newContexts     = IntMap.insert end newContextEnd values
 
                          in if (oldContextEnd /= newContextEnd) --Context value changed?
                             then solve' analysis program newWorklist newContexts
@@ -79,15 +76,15 @@ class (Ord property, Show property, Eq property) => Analysis analysis property |
 
 {-
 forEachContext :: (Label -> a -> b) -> Values a -> Values b
-forEachContext func = IM.mapWithKey (\lab context -> Map.map (func lab) context)
+forEachContext func = IntMap.mapWithKey (\lab context -> Map.map (func lab) context)
 -}
 
-fixPoint::(Eq a) => (a -> a) -> a -> a
-fixPoint f a | a == na   = a
-             | otherwise = fixPoint f na
-                where na = f a
+fixPoint :: (Eq a) => (a -> a) -> a -> a
+fixPoint f a | a == a'   = a
+             | otherwise = fixPoint f a'
+             where a' = f a
         
 visualizeWContexts::(Show property) => Values property -> Program -> String -> IO ()
 visualizeWContexts values program file = 
-    let decorateNode (l, n) = [GV.Label (GV.StrLabel (show l ++ ":" ++ show n++" -- "++(show . fromJust .IM.lookup l $ values)))]
+    let decorateNode (l, n) = [GV.Label (GV.StrLabel (show l ++ ":" ++ show n++" -- "++(show . fromJust .IntMap.lookup l $ values)))]
     in visualizeProgramWInfo decorateNode file program
--- src/Ruud/LiveVariables.hs
+++
@@ -1,7 +0,0 @@
-module EmbellishedMonotoneFramework.Analysis.LiveVariables where
-
-newtype LiveVariables = LiveVariables ()
-
-instance EmbellishedMonotoneFramework LiveVariables () where
-    lHat = undefined
-
